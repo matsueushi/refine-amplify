f213ae2855d77a5f423de8fbd6bd099a6c9e7ac1
matsueushi <matsueushi@gmail.com>
2024-03-02 06:50:02 +0000

    with limit

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -69,7 +69,7 @@ describe("dataProvider", () => {
         const call = calls[0] as Array<unknown>;
 
         expect(call[0]).toBe("listResourcesQuery");
-        // expect(call[1]).toEqual({ limit: 10, nextToken: null });
+        expect(call[1]).toEqual({ limit: 10 });
 
         expect(result).toEqual({
             data: [
@@ -93,6 +93,55 @@ describe("dataProvider", () => {
             total: 2,
         });
     });
+
+    test("get list with limit", async () => {
+        const mockGraphql = jest.fn(async () => {
+            return {
+                listResources: {
+                    items: [
+                        {
+                            id: "1",
+                            name: "Resource 1",
+                            createdAt: '2024-02-26T09:26:44.908Z',
+                            updatedAt: '2024-02-26T09:26:44.908Z',
+                            owner: 'ownerId',
+                            __typename: 'Resource'
+                        },
+                    ],
+                    nextToken: "nextToken",
+                    __typename: "ModelResourceConnection",
+                },
+            };
+        });
+
+        const client = generateClient();
+
+        jest.spyOn(DataProvider.prototype, "graphql").mockImplementation(mockGraphql);
+        const provider = dataProvider(client, operations);
+
+        const result = await provider.getList({ resource: "resources", pagination: { current: 1, pageSize: 1 } });
+
+        const calls = mockGraphql.mock.calls;
+        const call = calls[0] as Array<unknown>;
+
+        expect(call[0]).toBe("listResourcesQuery");
+        expect(call[1]).toEqual({ limit: 1 });
+
+        expect(result).toEqual({
+            data: [
+                {
+                    id: "1",
+                    name: "Resource 1",
+                    createdAt: '2024-02-26T09:26:44.908Z',
+                    updatedAt: '2024-02-26T09:26:44.908Z',
+                    owner: 'ownerId',
+                    __typename: 'Resource'
+                },
+            ],
+            total: 2,
+        });
+    }
+    );
 
     test("create resource", async () => {
         const mockGraphql = jest.fn(async () => {
